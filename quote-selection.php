de63b8a4cc4d646e6917aa2883d48f193be42d8a
Kailey Lampert <trepmal@gmail.com>
2017-01-08 19:51:56 -0800

    fix text domain, clean up javascript

--- quote-selection.php
+++ quote-selection.php
@@ -17,19 +17,18 @@ $quote_selection = new Quote_Selection();
 
 class Quote_Selection {
 
-	var $textdomain = 'quote-selection';
-
 	function __construct() {
 		add_action( 'comment_form_top', array( $this, 'comment_form_top' ) );
 		add_action( 'comment_form_top', array( $this, 'comment_form_top_scripts' ), 11 );
 	}
 
 	function comment_form_top() {
-		echo '<button id="quote-selection">Quote Selection</button>';
+		echo '<button id="quote-selection">' . esc_html__( 'Quote Selection', 'quote-selection' ) . '</button>';
 	}
 
 	function comment_form_top_scripts() {
 		wp_enqueue_script('jquery');
+		global $allowedtags;
 		?>
 <script>
 jQuery('#quote-selection').on('click', function(ev) {
@@ -40,7 +39,8 @@ jQuery('#quote-selection').on('click', function(ev) {
 		commentform = jQuery( document.getElementById('comment') ),
 		commenttext = commentform.val(),
 		padleft     = "\n",
-		padright    = "\n";
+		padright    = "\n",
+		blockquote  = <?php echo isset( $allowedtags['blockquote'] ) ? 'true' : 'false'; ?>;
 
 	if ( '' == commenttext ) {
 		padleft = '';
@@ -53,15 +53,10 @@ jQuery('#quote-selection').on('click', function(ev) {
 		return true;
 	}
 
-	<?php
-		// only wrap in '<blockquote>' if that tag is allowed
-		// @todo Make tag customizable
-		global $allowedtags;
-		if ( isset( $allowedtags['blockquote'] ) ) :
-	?>
-	// wrap
-	quote = '<blockquote>' + selection + '</blockquote>';
-	<?php endif; // blockquote in $allowedtags ?>
+	if ( blockquote ) {
+		// wrap
+		quote = '<blockquote>' + selection + '</blockquote>';
+	}
 
 	quote = padleft + quote + padright;
 
